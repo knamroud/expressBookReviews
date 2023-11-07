c72d18d1373cf59e9d05762f0e1b8a47c606d521
Kirelos Namroud <namroudkirelo04@gmail.com>
2023-11-07 21:34:05 +0100

    booksdb.js is now an express app exposing books endpoints

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,3 +1,4 @@
+const express = require('express');
 let books = {
       1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
@@ -11,4 +12,39 @@ let books = {
       10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
-module.exports=books;
+const PORT = 5001;
+
+app = express();
+
+
+// Get the book list available in the shop
+app.get('/',function (req, res) {
+      return res.status(200).json(books);
+});
+    
+// Get book details based on ISBN
+app.get('/isbn/:isbn',function (req, res) {
+      return res.status(200).json(books[req.params.isbn]);
+});
+
+// Get book details based on author
+app.get('/author/:author',function (req, res) {
+      let filtered = Object.keys(books)
+      .filter(key => books[key].author === req.params.author)
+      .reduce((obj, key) => {
+        obj[key] = books[key];
+        return obj;
+      }, {});
+      return res.status(200).json(filtered);
+    });
+    
+// Get all books based on title
+app.get('/title/:title',function (req, res) {
+      let filtered = Object.keys(books)
+      .filter(key => books[key].title === req.params.title)
+      .reduce((obj, key) => {
+            obj[key] = books[key];
+            return obj;
+      }, {});
+      return res.status(200).json(filtered);
+});
